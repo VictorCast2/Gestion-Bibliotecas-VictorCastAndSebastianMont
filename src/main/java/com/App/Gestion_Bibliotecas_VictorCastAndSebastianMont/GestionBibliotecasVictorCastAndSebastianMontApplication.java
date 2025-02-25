cf953b59c78b839fd812cbd414425ef44b844a0b
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-25 16:13:37 -0500

    Update

---
+++ src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/GestionBibliotecasVictorCastAndSebastianMontApplication.java
@@ -0,0 +1,13 @@
+package com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibliotecasVictorCastAndSebastianMontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBibliotecasVictorCastAndSebastianMontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/GestionBibliotecasVictorCastAndSebastianMontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBibliotecasVictorCastAndSebastianMontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
