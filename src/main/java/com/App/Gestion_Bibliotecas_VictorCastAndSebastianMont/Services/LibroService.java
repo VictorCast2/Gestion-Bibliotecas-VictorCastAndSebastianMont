2481ac3f1dabbb30273323dc9c850dcfc44477dd
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-25 17:59:50 -0500

    Update LibroService.java

--- src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/Services/LibroService.java
+++ src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/Services/LibroService.java
@@ -3,6 +3,7 @@ package com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont.Services;
 import com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont.Model.LibroModel;
 import com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont.Repository.LibroRepository;
 import lombok.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -17,12 +18,27 @@ public class LibroService {
         return libroRepository.findAll();
     }
 
+    @PreAuthorize("hasRole('Admin')")
     public void guardarLibro(LibroModel libro) {
         libroRepository.save(libro);
     }
 
+    @PreAuthorize("hasRole('Admin')")
     public void eliminarLibro(Long id) {
         libroRepository.deleteById(id);
     }
 
+    @PreAuthorize("hasRole('Admin')")
+    public void actualizarLibro(LibroModel libro) {
+    }
+
+    @PreAuthorize("hasRole('User')")
+    public List<LibroModel> buscarLibro() {
+        return libroRepository.findAll();
+    }
+
+    @PreAuthorize("hasRole('User')")
+    public void solicitarPrestamoLibro() {
+    }
+    
 }
