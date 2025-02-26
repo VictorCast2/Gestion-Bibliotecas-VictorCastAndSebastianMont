fd44312af94c13fa0726a657b38ec94f6fe309b6
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-25 19:36:03 -0500

    Update

--- src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/Configuration/ConfigurationSegurity.java
+++ src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/Configuration/ConfigurationSegurity.java
@@ -38,7 +38,7 @@ public class ConfigurationSegurity {
                 .csrf(csrf -> csrf.disable()) // Deshabilita CSRF
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/Api/Auth/Login", "/Api/Auth/Logout").permitAll() // Permite acceso público
-                        .requestMatchers("/Css/**", "/Js/**", "/Img/**").permitAll() // Permite acceso público
+                        .requestMatchers("/Css/**", "/Img/**", "/Js/**").permitAll() // Permite acceso público
                         .requestMatchers("/Error/**", "/Error").permitAll()
                         .requestMatchers("/Api/Admin/**").hasRole("Admin") // Autenticación para rutas de administrador
                         .requestMatchers("/Api/User/**").hasRole("User") // Autenticación para rutas de usuario
@@ -73,7 +73,9 @@ public class ConfigurationSegurity {
      */
     @Bean
     public AuthenticationSuccessHandler successHandler() {
-        return (HttpServletRequest _, HttpServletResponse response, Authentication authentication) -> {
+        return (HttpServletRequest request, HttpServletResponse response, Authentication authentication) -> {
+            System.out.println("Entró al successHandler");
+
             if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_Admin"))) {
                 System.out.println("Llego el Admin");
                 response.sendRedirect("/Api/Admin/Libros");
@@ -96,10 +98,6 @@ public class ConfigurationSegurity {
         return new CustomUserDetailsService(usuarioRepository);
     }
 
-    /**
-     * Configura el encriptador de contraseñas.
-     * @return El encriptador de contraseñas BCrypt.
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -115,7 +113,7 @@ public class ConfigurationSegurity {
     public DaoAuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
+        authProvider.setPasswordEncoder(passwordEncoder); // Aquí lo usa correctamente
         return authProvider;
     }
 
