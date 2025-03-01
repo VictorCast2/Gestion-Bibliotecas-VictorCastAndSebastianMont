7bb0c2697e1b5d62b68fc79aec2fa6124f64346d
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-02-28 21:22:10 -0500

    Update UserServices.java

--- src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/Services/UserServices.java
+++ src/main/java/com/App/Gestion_Bibliotecas_VictorCastAndSebastianMont/Services/UserServices.java
@@ -1,5 +1,6 @@
 package com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont.Services;
 
+import com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont.Model.UserModel;
 import com.App.Gestion_Bibliotecas_VictorCastAndSebastianMont.Repository.UserRepository;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +13,24 @@ public class UserServices {
 
     @Autowired
     private UserRepository userRepository;
+
+    /**
+     * Registra un usuario en la base de datos
+     * @param user
+     */
+    public void registrarUser(UserModel user){
+        userRepository.save(user);
+    }
+
+    /**
+     * Elimina un usuario de la base de datos
+     */
+    public void modificarUser(UserModel user){
+        if(userRepository.existsById(user.getId())) {
+            userRepository.save(user);
+        } else {
+            throw new IllegalArgumentException("El usuario no existe ... ");
+        }
+    }
 
 }
